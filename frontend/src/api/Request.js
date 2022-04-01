05c2dd491afcdba91b399a8fa0958f68d3262170
Ju Hyeon <aganga7427@gmail.com>
2022-04-01 20:03:34 +0900

    FIX: 관리자 페이지 기능 구현

--- frontend/src/api/Request.js
+++ frontend/src/api/Request.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 const BASE_URL = 'http://j6b108.p.ssafy.io:9090/'
 // const BASE_URL = 'http://localhost:8080/'
 
-export async function postReqeust(url, data) {
+export async function postRequest(url, data) {
   return await axios.post(BASE_URL + url, data)
+}
+
+export async function getRequest(url, params) {
+  return await axios.get(BASE_URL + url + params)
 }
