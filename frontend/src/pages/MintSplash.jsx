fac3d7a1637696e19f7a029cd656bf59670ff819
maltepoo <jaengpin@gmail.com>
2022-04-03 18:42:32 +0900

    DESIGN: 스플래시 스크린 색상 변경

--- frontend/src/pages/MintSplash.jsx
+++ frontend/src/pages/MintSplash.jsx
@@ -1,30 +1,34 @@
 import { Box, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 export default function MintSplash() {
+  const navigate = useNavigate()
   const nextPage = () => {
-    window.location.pathname = '/intro'
+    // window.location.pathname = '/intro'
+    navigate('/intro')
   }
 
   return (
-    <div style={style} onMouseUp={nextPage}>
+    <Box sx={style} onMouseUp={nextPage}>
       <Box sx={{ textAlign: 'center' }}>
         <img src="do-duck.gif" width="150px" />
       </Box>
       <Box sx={{ textAlign: 'center' }}>
-        <img src="logo_light.png" alt="" width="100px" />
+        <img src="logo_dark.png" alt="" width="100px" />
       </Box>
       <Box>
-        <Typography sx={{ textAlign: 'center', mt: '10px', color: '#000000' }}>너에게 스며드는 시간</Typography>
+        <Typography sx={{ textAlign: 'center', mt: '10px', color: '#fff' }}>너에게 스며드는 시간</Typography>
       </Box>
       <Box>
-        <Typography sx={{ color: '#80808026', mt: '5px' }}>아무 곳이나 클릭하세요</Typography>
+        <Typography sx={{ color: 'rgba(255,255,255,0.2)', mt: '22px' }}>아무 곳이나 클릭하세요</Typography>
       </Box>
-    </div>
+    </Box>
   )
 }
 
 // style
 const style = {
+  backgroundColor: '#8811DD',
   display: 'flex',
   flexDirection: 'column',
   height: '100vh',
