50ea63a1c0b54329100da0d6bf19d1e4441f9207
maltepoo <jaengpin@gmail.com>
2022-04-04 11:35:13 +0900

    FEAT: 모달 팝업방식 변경, groupbtn 함수 추가 S06P22B108-140

--- frontend/src/components/common/MintBtnGroup.jsx
+++ frontend/src/components/common/MintBtnGroup.jsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { Box, Button } from '@mui/material'
 
 //prev = {url: , content: 버튼 텍스트, color: 버튼 색상}
-//next = {url: , content: 버튼 텍스트, color: 버튼 색상}
+//next = {url: , content: 버튼 텍스트, color: 버튼 색상, handleClick: 사용할 함수}
 //params = 라우팅시 사용할 파라미터(Optional)
 
 function MintBtnGroup({ position, prev, next, passData }) {
@@ -13,7 +13,11 @@ function MintBtnGroup({ position, prev, next, passData }) {
     prev ? navigate(prev.url, { state: { ...passData } }) : alert('이동할 다음 주소가 없습니다')
   }
   const handleNext = () => {
-    next ? navigate(next.url, { state: { ...passData } }) : alert('이동할 다음 주소가 없습니다')
+    if (next.handleClick) {
+      next.handleClick()
+    } else {
+      next ? navigate(next.url, { state: { ...passData } }) : alert('이동할 다음 주소가 없습니다')
+    }
   }
   return (
     <Box
