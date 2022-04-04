8141c239ca9ea2051004aa8780cf637c20962619
maltepoo <jaengpin@gmail.com>
2022-04-05 00:53:17 +0900

    DESIGN: 거래페이지 레이아웃 수정 S06P22B108-146

--- frontend/src/components/common/MintCard.jsx
+++ frontend/src/components/common/MintCard.jsx
@@ -1,5 +1,6 @@
 import { Card, CardMedia, CardActions, Typography, Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import '../../styles/MintTradeContents.css'
 
 export default function MintCard({ cardData, type }) {
   const navigate = useNavigate()
@@ -19,7 +20,9 @@ export default function MintCard({ cardData, type }) {
         boxSizing: 'border-box',
       }}
       onClick={handleDetail}>
-      <Card sx={{ border: '1px solid grey' }}>
+      <Card
+        className="tradeCard__container"
+        sx={{ border: '1px solid rgba(0, 0, 0, 0.2)', boxShadow: '0', cursor: 'pointer' }}>
         <CardMedia component="img" image={cardData.imgUrl} alt="nft사진" sx={{ height: '175px' }} />
         {type === 'trade' ? (
           <CardActions sx={{ padding: '1px', height: '30px' }}>
