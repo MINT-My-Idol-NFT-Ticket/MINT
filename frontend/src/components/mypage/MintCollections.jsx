34857dba30a281ac344281ea330327f40491d1fc
pch1656 <pch1656@gmail.com>
2022-04-05 00:05:57 +0900

    REFACTOR: MintCard 수정
    MintTradeCard 삭제

--- frontend/src/components/mypage/MintCollections.jsx
+++ frontend/src/components/mypage/MintCollections.jsx
@@ -1,3 +1,12 @@
-export default function MintCollections() {
-  return <></>
+import { Grid } from '@mui/material'
+
+export default function MintCollections({ tokenIds }) {
+  const collectionList = () => {
+    tokenIds.map()
+  }
+  return (
+    <Grid container sx={{ border: '1px solid red' }}>
+      {collectionList}
+    </Grid>
+  )
 }
--- frontend/src/components/trade/MintTradeCard.jsx
+++
@@ -1,5 +0,0 @@
-import MintCard from '../common/MintCard'
-
-export default function MintTradeCard({ cardData }) {
-  return <MintCard cardData={cardData} type="trade" />
-}
