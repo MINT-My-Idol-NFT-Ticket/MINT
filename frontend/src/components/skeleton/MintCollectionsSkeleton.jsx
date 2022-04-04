f40a998bbe36efcd021b67b056850ffbce243a5d
maltepoo <jaengpin@gmail.com>
2022-04-05 00:22:03 +0900

    DESIGN: 마이페이지 컬렉션탭 반응형오류 수정

--- frontend/src/components/skeleton/MintCollectionsSkeleton.jsx
+++ frontend/src/components/skeleton/MintCollectionsSkeleton.jsx
@@ -1,19 +1,9 @@
 import { Box, Grid, Skeleton } from '@mui/material'
 import React from 'react'
 
-/*
-{notOpenConcerts.length === 0 ? (
-          <Grid container spacing={2.5}>
-            {[0, 0, 0, 0].map((v, i) => (
-              <Grid key={i} item xs={6}>
-                <MintVerticalSkeleton notOpen={true} />
-              </Grid>
-            ))}
-          </Grid>
-*/
 function MintCollectionsSkeleton(props) {
   return (
-    <Grid container spacing={{ xs: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    <Grid container spacing={{ xs: 2 }} columns={{ xs: 12 }}>
       {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
         <Grid item key={i} xs={4}>
           <Skeleton variant="rectangular" sx={skeletonStyle} />
