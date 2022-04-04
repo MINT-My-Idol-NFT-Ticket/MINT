a565a2d9bd40569c63cf8c298b8ecef8350dc4f5
Ju Hyeon <aganga7427@gmail.com>
2022-04-04 22:04:19 +0900

    FEAT: 결제창 예외 처리 S06P22B108-135
    항목 미선택 시 알림 기능 개발
    결제 실패 시 마이페이지 이동
    결제 성공 시 홈페이지 이동

--- frontend/src/functions/alert/alertFunctions.js
+++ frontend/src/functions/alert/alertFunctions.js
@@ -3,6 +3,7 @@ import withReactContent from 'sweetalert2-react-content'
 import '../../styles/MintAlert.css'
 
 import generateConfirm from './generateConfirm.js'
+import generateConfirmNoBtn from './generateConfirmNoBtn.js'
 import generateCheck from './generateCheck.js'
 import generateError from './generateError.js'
 import generateTimer from './generateTimer.js'
@@ -10,6 +11,7 @@ import generateTimer from './generateTimer.js'
 const swal = withReactContent(Swal)
 
 export const confirmMessage = (title, text, callback, bright) => generateConfirm(swal, title, text, callback, bright)
+export const confirmMessageNoBtn = (title, callback, bright) => generateConfirmNoBtn(swal, title, callback, bright)
 export const checkMessage = (title, callback, bright) => generateCheck(swal, title, callback, bright)
 export const errorMessage = (title, callback, bright) => generateError(swal, title, callback, bright)
 export const timerMessage = (title, html, callback, bright) => generateTimer(swal, title, html, callback, bright)
