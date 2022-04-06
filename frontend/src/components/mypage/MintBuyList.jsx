f65bf48d1c12b6e4b50b67c36d72c8bc15a39a1f
Ju Hyeon <aganga7427@gmail.com>
2022-04-06 23:40:41 +0900

    DESIGN: 마이페이지 빈 리스트 레이아웃 추가 S06P22B108-104

--- frontend/src/components/mypage/MintBuyList.jsx
+++ frontend/src/components/mypage/MintBuyList.jsx
@@ -1,14 +1,12 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-import { burnTicket } from '../../functions/erc/ERCfunctions'
 
 import MintHorizontalCard from '../common/MintHorizontalCard'
 import MintHorizontalSkeleton from '../skeleton/MintHorizontalSkeleton'
 import MintCancelModal from './MintCancelModal'
 
-export default function MintBuyList({ contractList }) {
+export default function MintBuyList({ contractList, loading }) {
   const navigate = useNavigate()
 
   const [payOpen, setPayOpen] = useState(false)
@@ -29,19 +27,36 @@ export default function MintBuyList({ contractList }) {
 
   return (
     <Box>
-      {contractList.length === 0
-        ? [0, 0, 0, 0, 0].map((v, i) => <MintHorizontalSkeleton key={i} notOpen={false} />)
-        : contractList &&
-          contractList.map((concert, index) => (
-            <MintHorizontalCard
-              key={`${concert.id}-${concert.startDate}-${index}`}
-              concertData={concert}
-              passDetail={handleNavigation}>
-              <Button variant="outlined" size="small" sx={{ width: '45%' }} onClick={() => handlePayOpen(concert.id)}>
-                예매 취소
-              </Button>
-            </MintHorizontalCard>
-          ))}
+      {loading ? (
+        [0, 0, 0, 0, 0].map((v, i) => <MintHorizontalSkeleton key={i} notOpen={false} />)
+      ) : (
+        <>
+          {contractList.length === 0 ? (
+            <Typography
+              sx={{
+                color: 'text.primary',
+                opacity: '.5',
+                position: 'absolute',
+                top: '55%',
+                left: '50%',
+                transform: 'translate(-50%)',
+              }}>
+              구매 내역이 없습니다
+            </Typography>
+          ) : (
+            contractList.map((concert, index) => (
+              <MintHorizontalCard
+                key={`${concert.id}-${concert.startDate}-${index}`}
+                concertData={concert}
+                passDetail={handleNavigation}>
+                <Button variant="outlined" size="small" sx={{ width: '45%' }} onClick={() => handlePayOpen(concert.id)}>
+                  예매 취소
+                </Button>
+              </MintHorizontalCard>
+            ))
+          )}
+        </>
+      )}
       <MintCancelModal open={payOpen} handleClose={handlePayClose} targetConcertId={targetConcertId} />
     </Box>
   )
