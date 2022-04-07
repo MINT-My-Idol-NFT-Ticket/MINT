1d81b2baf44baa94c020bcd6244e0d22324056fc
pch1656 <pch1656@gmail.com>
2022-04-07 13:25:36 +0900

    FEAT: 티켓 거래 목록 표시  S06P22B108-139
    판매목록 트랜잭션, 목록 표시 기능 완료

--- frontend/src/components/common/MintCard.jsx
+++ frontend/src/components/common/MintCard.jsx
@@ -2,48 +2,30 @@ import { Card, CardMedia, CardActions, Typography, Box } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { getTokenURI } from '../../functions/erc/ERCfunctions'
-import { getRequest } from '../../api/requests'
-
 import MintCollectionSingleSkeletion from '../skeleton/MintCollectionSingleSkeletion'
 
-export default function MintCard2({ tokenId }) {
-  const [tokenURI, setTokenURI] = useState(null)
+export default function MintCard2({ tokenURI }) {
   const navigate = useNavigate()
-  console.log(tokenId)
   console.log(tokenURI)
   const showTicket = () => {
     navigate(`/trade/ticket`, { state: { ...tokenURI.data } })
   }
-  const getURI = async () => {
-    try {
-      const uri = await getTokenURI(tokenId.contractAddress, tokenId.tokenId)
-      let response = ''
-      if (uri) response = await getRequest(uri)
-      // if (uri) response = await getRequest(`/api/ticket/uriData/${uri}`)
-
-      setTokenURI(response)
-    } catch {
-      navigate('/error404')
-    }
-  }
-
-  useEffect(() => {
-    getURI()
-  }, [])
 
   return (
     <Box onClick={showTicket}>
-      {tokenURI === null ? (
+      {tokenURI === undefined ? (
         <MintCollectionSingleSkeletion />
       ) : (
-        <Card>
-          {tokenURI !== '' ? (
-            <></>
-          ) : (
-            <CardMedia component="img" image={JSON.parse(tokenId.img).gif} alt="nft사진" sx={{ height: '175px' }} />
-          )}
-        </Card>
+        <>
+          <Card sx={{ height: '400px' }}>
+            {tokenURI === {} ? (
+              <></>
+            ) : (
+              <CardMedia component="img" image={JSON.parse(tokenURI.img).gif} alt="nft사진" sx={{ height: '175px' }} />
+            )}
+          </Card>
+          <Typography>가격</Typography>
+        </>
       )}
     </Box>
   )
