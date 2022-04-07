6c6fb769ead05802503efce8b561b73526d52943
pch1656 <pch1656@gmail.com>
2022-04-07 14:36:14 +0900

    FIX: 판매 NFT 가격 표시 수정중

--- frontend/src/components/common/MintCard.jsx
+++ frontend/src/components/common/MintCard.jsx
@@ -1,15 +1,26 @@
 import { Card, CardMedia, CardActions, Typography, Box } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { getSaleTicketPrice } from '../../functions/erc/ERCfunctions'
 
 import MintCollectionSingleSkeletion from '../skeleton/MintCollectionSingleSkeletion'
 
 export default function MintCard2({ tokenURI }) {
   const navigate = useNavigate()
+  const [price, setPrice] = useState(null)
   console.log(tokenURI)
   const showTicket = () => {
     navigate(`/trade/ticket`, { state: { ...tokenURI.data } })
   }
+  const getPrice = async () => {
+    console.log(tokenURI.saleContract, tokenURI.tokenId)
+    const response = await getSaleTicketPrice(tokenURI.saleContract, tokenURI.tokenId)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    getPrice()
+  }, [])
 
   return (
     <Box onClick={showTicket}>
@@ -17,14 +28,21 @@ export default function MintCard2({ tokenURI }) {
         <MintCollectionSingleSkeletion />
       ) : (
         <>
-          <Card sx={{ height: '400px' }}>
+          <Card>
             {tokenURI === {} ? (
               <></>
             ) : (
-              <CardMedia component="img" image={JSON.parse(tokenURI.img).gif} alt="nft사진" sx={{ height: '175px' }} />
+              <>
+                <CardMedia
+                  component="img"
+                  image={JSON.parse(tokenURI.img).gif}
+                  alt="nft사진"
+                  sx={{ height: '175px' }}
+                />
+                <Typography>t</Typography>
+              </>
             )}
           </Card>
-          <Typography>가격</Typography>
         </>
       )}
     </Box>
